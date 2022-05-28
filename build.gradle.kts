55ced24b0baa77d978bdf3e865bfd27edc8e9c0a
Craig Miller <craigmiller160@gmail.com>
2022-05-28 16:18:17 -0400

    Got spotless working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.diffplug.gradle.spotless.SpotlessExtension
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    id("com.diffplug.spotless") version "6.6.1"
 }
 
 group = "io.craigmiller160"
@@ -9,6 +11,12 @@ version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+configure<SpotlessExtension> {
+    kotlin {
+        ktfmt()
+    }
 }
 
 dependencies {
