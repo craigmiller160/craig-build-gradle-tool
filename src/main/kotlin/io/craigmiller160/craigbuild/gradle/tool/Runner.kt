e69c90f1c5eba616a0a2f50c940a8d52206d4fac
Craig Miller <craigmiller160@gmail.com>
2022-05-28 16:31:07 -0400

    Beginning to implement tool

--- src/main/kotlin/io/craigmiller160/craigbuild/gradle/tool/Runner.kt
+++ src/main/kotlin/io/craigmiller160/craigbuild/gradle/tool/Runner.kt
@@ -1,3 +1,20 @@
 package io.craigmiller160.craigbuild.gradle.tool
 
-fun main(args: Array<String>) {}
+import io.craigmiller160.craigbuild.gradle.plugin.model.CraigBuildProject
+import java.io.File
+import org.gradle.tooling.GradleConnector
+import org.gradle.tooling.model.idea.IdeaProject
+
+fun main(args: Array<String>) {
+  if (args.isEmpty()) {
+    throw RuntimeException("Must provide project file path")
+  }
+
+  val (craigBuildProject, ideaProject) =
+      GradleConnector.newConnector().forProjectDirectory(File(args.first())).connect().use {
+          connection ->
+        Pair(
+            connection.getModel(CraigBuildProject::class.java),
+            connection.getModel(IdeaProject::class.java))
+      }
+}
