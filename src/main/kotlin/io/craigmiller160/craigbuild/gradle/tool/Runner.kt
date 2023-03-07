86ed398fc390baa6c38ac08e88c3c9c8367d9ea0
Craig Miller <craigmiller160@gmail.com>
2023-03-07 17:13:41 -0500

    Got everything working

--- src/main/kotlin/io/craigmiller160/craigbuild/gradle/tool/Runner.kt
+++ src/main/kotlin/io/craigmiller160/craigbuild/gradle/tool/Runner.kt
@@ -1,19 +1,13 @@
 package io.craigmiller160.craigbuild.gradle.tool
 
-import arrow.core.Either
-import arrow.core.getOrHandle
-import arrow.core.sequence
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.craigmiller160.craigbuild.gradle.plugin.model.CraigBuildProject
-import io.craigmiller160.craigbuild.gradle.tool.error.UnresolvedDependencyException
 import io.craigmiller160.craigbuild.gradle.tool.model.Item
 import io.craigmiller160.craigbuild.gradle.tool.model.Project
 import java.io.File
 import org.gradle.tooling.GradleConnector
 import org.gradle.tooling.model.ExternalDependency
 import org.gradle.tooling.model.idea.IdeaProject
-
-private val GRADLE_API_REGEX = Regex("^.*\\/gradle-api-.*\\.jar$")
 
 fun main(args: Array<String>) {
   if (args.isEmpty()) {
@@ -46,17 +40,7 @@ private fun getDependencies(model: IdeaProject): List<Item> =
         .flatMap { it.dependencies.all.asSequence() }
         .filter { it is ExternalDependency }
         .map { it as ExternalDependency }
-        .map { dependency ->
-          when {
-            dependency.gradleModuleVersion != null -> Either.Right(dependency.gradleModuleVersion)
-            GRADLE_API_REGEX.matches(dependency.file.absolutePath) -> Either.Right(null)
-            else -> Either.Left(UnresolvedDependencyException(dependency.file.absolutePath))
-          }
-        }
-        .sequence()
-        .map { dependencies ->
-          dependencies.filterNotNull().map {
-            Item(group = it.group, name = it.name, version = it.version)
-          }
-        }
-        .getOrHandle { throw it }
+        .map { dependency -> dependency.gradleModuleVersion }
+        .filterNotNull()
+        .map { Item(group = it.group, name = it.name, version = it.version) }
+        .toList()
